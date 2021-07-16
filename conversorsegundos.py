8f3bc6cc8fe1060edce7062eed1161ae3d976d0c
giovannarigolei <68572002+giovannarigolei@users.noreply.github.com>
2021-07-16 14:58:42 -0300

    Add files via upload

---
+++ conversorsegundos.py
@@ -0,0 +1,13 @@
+segundos_str=input("Por favor, digite o número de segundos que deseja converter: ")
+total_segs=int(segundos_str)
+
+dias=total_segs//86400
+segs_restantes=total_segs%86400
+
+horas=segs_restantes//3600
+segs_restantes_a=segs_restantes%3600
+
+minutos=segs_restantes_a//60
+segs_restantes_final=segs_restantes_a%60
+
+print(dias, "dias,", horas, "horas,", minutos, "minutos e", segs_restantes_final, "segundos." )
